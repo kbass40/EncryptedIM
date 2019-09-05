fbe74223506c295bd001c0422b6106c6f48e12b8
Kyle Bassignani <kylewbassignani@ufl.edu>
2019-09-05 10:27:12 -0400

    added some comments

--- UnencryptedIM.py
+++ UnencryptedIM.py
@@ -27,37 +27,50 @@ else:
     invalidArguments("invalid arguments")
 
 def server():
+    # create socket
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     server_socket.bind((hostname, portNumber))
+    # listen for incoming connections
     server_socket.listen()
     socket_list = [server_socket]
     client_socket, addr = server_socket.accept()
 
     while True:
         socket_list = [sys.stdin, client_socket]
+        # wait for IO
         read, write, error = select.select(socket_list, [], [])
         for sock in read:
+            # recieve message
             if sock == client_socket:
                 message = ""
                 newMessage = True
                 while True:
                     msg = client_socket.recv(512)
+                    # new message determine the length from the header
                     if newMessage:
                         length = int(msg[:HEADERSIZE])
                         newMessage = False
+                    # append message to needed for large messages
                     message += msg.decode("utf-8")
+                    # print message once the entire message has been recieved
                     if len(message) - HEADERSIZE == length:
                         print(message[HEADERSIZE:])
+                        # reset values and break loop
                         newMessage = True
                         message = ""
                         break
+            # send message
             else:
+                # read from standard input
                 msg = input()
+                # attach header to message
                 msg = f"{len(msg):<{HEADERSIZE}}" + msg
+                # send message
                 client_socket.send(bytes(msg, "utf-8"))
     server_socket.close()
 
+# client socket works in a very similar way to server socket with the exception of the initial connection
 def client():
     client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     client_socket.connect((hostname, portNumber))
